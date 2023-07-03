2e7ee16f9152e0c3f90e3fcceeca289d422d89f5
escalopa <ahmad.helaly.dev@gmail.com>
2023-07-03 04:08:25 +0300

    docs: add example code

---
+++ example/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/escalopa/memcache"
+)
+
+func main() {
+	nodes := 1_000
+	capacity := 1_000_000
+	mc := memcache.New(nodes, capacity, memcache.WithLRU) // Or use `memcache.WithLFU`
+
+	var value interface{}
+	var ok bool
+
+	value, ok = mc.Get("foo")
+	fmt.Println(value, ok)
+	// Output: <nil> false
+
+	mc.Set("foo", "bar", 0)
+	value, ok = mc.Get("foo")
+	fmt.Println(value, ok)
+	// Output: bar true
+
+	mc.Delete("foo")
+	value, ok = mc.Get("foo")
+	fmt.Println(value, ok)
+	// Output: <nil> false
+
+	mc.Set("foo", "bar", 0)
+	value, ok = mc.Get("foo")
+	fmt.Println(value, ok)
+	// Output: bar true
+}
